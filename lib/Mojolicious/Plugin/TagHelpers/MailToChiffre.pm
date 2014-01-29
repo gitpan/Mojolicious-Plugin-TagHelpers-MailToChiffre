1cd75b501fce918ffa86a0364f2931efbe0ac024
Nils Diewald <akron@cpan.org>
2014-01-29 18:37:48 +0000

    Import of AKRON/Mojolicious-Plugin-TagHelpers-MailToChiffre-0.03 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-TagHelpers-MailToChiffre
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         AKRON/Mojolicious-Plugin-TagHelpers-MailToChiffre-0.03.tar.gz
    gitpan-cpan-author:       AKRON
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/TagHelpers/MailToChiffre.pm
+++ lib/Mojolicious/Plugin/TagHelpers/MailToChiffre.pm
@@ -4,7 +4,7 @@ use Mojo::ByteStream 'b';
 use Mojo::Collection 'c';
 use Mojo::URL;
 
-our $VERSION = '0.02_3';
+our $VERSION = '0.03';
 
 # Cache for generated CSS and JavaScript
 has [qw/js css/];
